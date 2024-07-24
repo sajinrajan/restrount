eb2aef73a2c38b453f71678affdcc7eac6948b3e
sajinrajan <sajinrajan082@gmail.com>
2024-07-24 13:19:25 +0530

    java srp css 5

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ function closeMenuBar(){
 mobileDiv.style.display = "none"
 menuIcon.style.display = "flex"
 }
+
+
+// 
+/* <!-- SLIDER SHOW CONTACT START --> */
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/* <!-- SLIDER SHOW CONTACT END--> */
